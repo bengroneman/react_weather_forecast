78c933fa5451aacdc8d37240e6f11fc1948e3256
bengroneman <bluegroneman10@gmail.com>
2020-01-23 08:21:56 -0700

    updated gitignore - try catch block

--- src/fetchForecast.js
+++ src/fetchForecast.js
@@ -1,6 +1,11 @@
 export default async function fetchForecast(q) {
     // https://openweathermap.org/forecast5
     const url = `https://api.openweathermap.org/data/2.5/forecast?q=${encodeURIComponent(q)}&units=imperial&APPID=e2215f02d2e8308870ff32b020df1a13`;
-    const response = await fetch(url);
-    return await response.json();
+    try {
+        const response = await fetch(url);
+        return await response.json();
+    } catch (e) {
+        console.error(e);
+        return false;
+    }
 }
